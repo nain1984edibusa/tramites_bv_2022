67a50d8de6796598afeaec60c7665471d5e08737
unknown <edibusa_1991@hotmail.com>
2022-08-24 11:56:37 -0500

    cambios tramite 17 - tabla tipo analisis

--- _form_res/rf_17.php
+++ _form_res/rf_17.php
@@ -1,21 +1,60 @@
 <?php
-
-/* 
+/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+include_once("./modelo/cls17AnalisisQuimico.php");
+include_once("./modelo/clsTipoAnalisis.php");
+
+$analisisQuimico = new cls17AnalisisQuimico();
+$analisisQuimico->setTu_id($tespecifico["tu_id"]);
+$analisisQuimico17 = $analisisQuimico->analisisQuimicoPorTramite();
+
+$tipoAnalisis = new clsTipoAnalisis();
+$catalogoTipoAnalisis = $tipoAnalisis->tipoAnalisisSeleccionarTodo();
 ?>
 <div class="col-xs-12 col-sm-12 col-md-12">
     <div class="group-material">
         <span>Marco Legal <span class="sp-requerido">*</span></span>
-        <?php 
-        $marco_legal="";
-        if(isset ($trespuesta)){
-            $marco_legal=$trespuesta["tuc_marcolegal"];
+        <?php
+        $marco_legal = "";
+        if (isset($trespuesta)) {
+            $marco_legal = $trespuesta["tuc_marcolegal"];
         }
         ?>
-        <textarea id="marcolegal" name="marcolegal" rows="4" class="tooltips-general material-control" placeholder="" required="" data-toggle="tooltip" data-placement="top" title="Escriba el marco legal"><?php echo str_replace('<br />','\n',$marco_legal); ?></textarea>
+        <textarea id="marcolegal" name="marcolegal" rows="4" class="tooltips-general material-control" placeholder="" required="" data-toggle="tooltip" data-placement="top" title="Escriba el marco legal"><?php echo str_replace('<br />', '\n', $marco_legal); ?></textarea>
+
+        <table class="table table-bordered">
+            <thead class="btn-primary">
+                <tr>
+                    <th>Seleccionado</th>
+                    <th>Ensayo/Parámetro</th>
+                    <th>Nro. Items <br/> de ensayo</th>
+                    <th>Precio por <br/>ítem de ensayo US$</th>
+                    <th>Total <br/> a Pagar</th>
+                </tr>
+            </thead>
+            <tbody style="background-color:#fff;">
+                <?php foreach ($catalogoTipoAnalisis as $dato): ?>
+
+                    <tr>
+
+                        <td><input type="checkbox" name="asistencia[]" value="<?php echo $dato["id"] ?>"></td>
+                        <td><?php echo $dato["ta_concepto"] ?></td>
+                        <td><input type="number" name="numero_items" id="numero_items" value="<?php echo $dato["id"] ?>"></td>
+                        <!--<input id="cantidad" name="cantidad" type="number" class="tooltips-general material-control" placeholder="Por ejemplo: 10" required="" maxlength="70" data-toggle="tooltip" data-placement="top"  onKeyUp="this.value = this.value.toUpperCase();">title="Escriba su número de identificación"-->
+                        <td><?php echo $dato["ta_costo"] ?></td>
+                        <!--<td><?php echo $dato["ta_total_a_pagar"] ?></td>-->
+                        <td><input type="number" name="ta_total_a_pagar" id="ta_total_a_pagar" value="<?php echo $dato["id"] ?>"></td>
+
+                    </tr>
+
+                <?php endforeach ?>  
+            </tbody>
+        </table>
     </div>
 </div>
+
+
 
---
+++ modelo/clsTipoAnalisis.php
@@ -0,0 +1,93 @@
+<?php
+
+Class clsTipoAnalisis {
+
+    //    definio los campos de la tabla 
+    private $ta_id;
+    private $ta_concepto;
+    private $ta_estado;
+    private $ta_costo;
+    private $ta_seleccionado;
+    private $ta_numero_ensayos;
+    private $ta_total_a_pagar;
+
+    //////////////////////////////   funciones //////////////////////
+
+    public function getTa_id() {
+        return $this->ta_id;
+    }
+
+    public function getTa_concepto() {
+        return $this->ta_concepto;
+    }
+
+    public function getTa_estado() {
+        return $this->ta_estado;
+    }
+
+    public function getTa_costo() {
+        return $this->ta_costo;
+    }
+
+    public function setTa_id($ta_id): void {
+        $this->ta_id = $ta_id;
+    }
+
+    public function setTa_concepto($ta_concepto): void {
+        $this->ta_concepto = $ta_concepto;
+    }
+
+    public function setTa_estado($ta_estado): void {
+        $this->ta_estado = $ta_estado;
+    }
+
+    public function setTa_costo($ta_costo): void {
+        $this->ta_costo = $ta_costo;
+    }
+
+    public function getTa_seleccionado() {
+        return $this->ta_seleccionado;
+    }
+
+    public function getTa_numero_ensayos() {
+        return $this->ta_numero_ensayos;
+    }
+
+    public function setTa_seleccionado($ta_seleccionado): void {
+        $this->ta_seleccionado = $ta_seleccionado;
+    }
+
+    public function setTa_numero_ensayos($ta_numero_ensayos): void {
+        $this->ta_numero_ensayos = $ta_numero_ensayos;
+    }
+    public function getTa_total_a_pagar() {
+        return $this->ta_total_a_pagar;
+    }
+
+    public function setTa_total_a_pagar($ta_total_a_pagar): void {
+        $this->ta_total_a_pagar = $ta_total_a_pagar;
+    }
+
+        //////   seleccionar tipo bien cultural    ///////////////////
+    public function tipoAnalisisSeleccionarTodo() {
+        // abro conexión a bases de datos
+        $bd = Db::getInstance();
+        $sql = 'select ta_id, ta_concepto, ta_costo FROM _ct_tramite17_tipo_analisis';
+        $rsprv = $bd->ejecutar($sql);
+        //$bd->cerrar();
+        return $rsprv;
+    }
+
+    public function tipoBienCulturalSeleccionarActivos() {
+        // abro conexión a bases de datos
+        $bd = Db::getInstance();
+        $sql = "SELECT * FROM _ct_tramite4_tipo_bien_cultural WHERE tbc_estado = 'ACT'";
+        $rsprv = $bd->ejecutar($sql);
+        //$bd->cerrar();
+        return $rsprv;
+    }
+
+    /////////////////////////////    fin de provicnias      ///////////////////////
+}
+
+?>
